e5dbea04ec781e86f14dee0a184e6390406b0742
SaraSat <sara_sat93@hotmail.com>
2021-03-16 14:05:26 +0100

    añadido vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,10 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+import Vuetify from 'vuetify';
+
+Vue.use(Vuetify);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
