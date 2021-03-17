6dbb1b289797af8d8d0c9af11c9a0ae8c664272c
SaraSat <sara_sat93@hotmail.com>
2021-03-17 11:50:24 +0100

    listado

--- resources/js/components/Inicio.vue
+++ resources/js/components/Inicio.vue
@@ -1,8 +1,69 @@
-<template>
-<v-btn>HOlaaaaaaaaaaaaaaaaaaaaaa</v-btn>
 
+
+<template>
+    <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+        <v-row>
+            <v-col class="col col-12" id="title">
+                <h1>Gestión de Productos</h1>
+            </v-col> 
+        </v-row>  
+        <v-row>
+            <v-col>
+                <v-data-table
+                    :headers="headers"
+                    :items="productos"
+                    class="elevation-1"
+                    :search="search"
+                    :custom-filter="getFilter">
+                
+                </v-data-table>
+            </v-col>
+        </v-row>
+    </v-parallax>
 </template>
 
-<style>
+<script>
+export default {
+    name: 'inicio', 
 
+    data() {
+        return {
+            headers: [
+                {text: 'Nombre', value:'name', sortable:true},
+                {tex: 'Descripción', value: 'description', sortable:false},
+                {text: 'Precio', value: 'price', sortable:true}
+            ], 
+
+            search: '', 
+            productos: [],
+        }
+    },  
+
+    created() {
+        this.listar();
+    },
+
+    methods: {
+        async listar() {
+            const res = await axios.get('list');
+
+            this.productos = res.data;
+        },
+
+        getFilter(value, search, item) {
+            return value != null &&
+                search != null &&
+                typeof value === 'string' &&
+                value.toString().toLocaleUpperCase().indexOf(search) !== -1
+        }
+    }
+}
+</script>
+
+
+<style>
+ #title {
+     text-align: center;
+     margin-top: 5%;
+ }
 </style>
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::get('/list', 'App\Http\Controllers\ProductoController@index');
+
